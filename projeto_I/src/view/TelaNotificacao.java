2a26181a50e9c0c43e3822f7069de10ea2cf32b1
Sofia Pascoalini <Sofia.m.pascoalini@gmail.com>
2026-04-26 15:57:38 -0300

    commit 26/04 notificação redonda

--- projeto_I/src/view/TelaNotificacao.java
+++ projeto_I/src/view/TelaNotificacao.java
@@ -8,6 +8,9 @@ import javax.swing.JMenu;
 import net.miginfocom.swing.MigLayout;
 import javax.swing.JLabel;
 import javax.swing.JScrollPane;
+
+import Botao.PainelArredondado;
+
 import java.awt.Color;
 import java.awt.FlowLayout;
 import java.awt.Font;
@@ -70,12 +73,12 @@ public class TelaNotificacao extends JPanel {
 		        getClass().getResourceAsStream("/fontes/Jomhuria-Regular.ttf")
 		).deriveFont(20f);
 		
-		JPanel panel_2 = new JPanel();
+		PainelArredondado panel_2 = new PainelArredondado();
 		panel_2.setBackground(new Color(235, 219, 194));
 		panel_1.add(panel_2, "cell 0 0,grow");
 		panel_2.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_8 = new JPanel();
+		PainelArredondado panel_8 = new PainelArredondado ();
 		panel_8.setBackground(new Color(145, 131, 131));
 		panel_2.add(panel_8, BorderLayout.NORTH);
 		panel_8.setLayout(new CardLayout(0, 0));
@@ -86,17 +89,17 @@ public class TelaNotificacao extends JPanel {
 		panel_8.add(lbNo1, "name_9304160647600");
 		lbNo1.setFont(fonte2);
 		
-		JPanel panel_9 = new JPanel();
+		PainelArredondado panel_9 = new PainelArredondado();
 		panel_9.setBackground(new Color(235, 219, 194));
 		panel_2.add(panel_9, BorderLayout.CENTER);
 		panel_9.setLayout(new FlowLayout(FlowLayout.CENTER, 5, 5));
 		
-		JPanel panel_5 = new JPanel();
+		PainelArredondado panel_5 = new PainelArredondado();
 		panel_5.setBackground(new Color(235, 219, 194));
 		panel_1.add(panel_5, "cell 2 0,grow");
 		panel_5.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_14 = new JPanel();
+		PainelArredondado panel_14 = new PainelArredondado();
 		panel_14.setBackground(new Color(145, 131, 131));
 		panel_5.add(panel_14, BorderLayout.NORTH);
 		panel_14.setLayout(new CardLayout(0, 0));
@@ -107,16 +110,16 @@ public class TelaNotificacao extends JPanel {
 		panel_14.add(lbNo2, "name_9600362844400");
 		lbNo2.setFont(fonte2);
 		
-		JPanel panel_15 = new JPanel();
+		PainelArredondado panel_15 = new PainelArredondado();
 		panel_15.setBackground(new Color(235, 219, 194));
 		panel_5.add(panel_15, BorderLayout.CENTER);
 		
-		JPanel panel_3 = new JPanel();
+		PainelArredondado panel_3 = new PainelArredondado();
 		panel_3.setBackground(new Color(235, 219, 194));
 		panel_1.add(panel_3, "cell 0 2,grow");
 		panel_3.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_10 = new JPanel();
+		PainelArredondado panel_10 = new PainelArredondado();
 		panel_10.setBackground(new Color(145, 131, 131));
 		panel_3.add(panel_10, BorderLayout.NORTH);
 		panel_10.setLayout(new CardLayout(0, 0));
@@ -127,16 +130,16 @@ public class TelaNotificacao extends JPanel {
 		panel_10.add(lbNo3, "name_9655721472700");
 		lbNo3.setFont(fonte2);
 		
-		JPanel panel_11 = new JPanel();
+		PainelArredondado panel_11 = new PainelArredondado();
 		panel_11.setBackground(new Color(235, 219, 194));
 		panel_3.add(panel_11, BorderLayout.CENTER);
 		
-		JPanel panel_6 = new JPanel();
+		PainelArredondado panel_6 = new PainelArredondado();
 		panel_6.setBackground(new Color(235, 219, 194));
 		panel_1.add(panel_6, "cell 2 2,grow");
 		panel_6.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_16 = new JPanel();
+		PainelArredondado panel_16 = new PainelArredondado();
 		panel_16.setBackground(new Color(145, 131, 131));
 		panel_6.add(panel_16, BorderLayout.NORTH);
 		panel_16.setLayout(new CardLayout(0, 0));
@@ -147,16 +150,16 @@ public class TelaNotificacao extends JPanel {
 		panel_16.add(lbNo4, "name_9790329387800");
 		lbNo4.setFont(fonte2);
 		
-		JPanel panel_17 = new JPanel();
+		PainelArredondado panel_17 = new PainelArredondado();
 		panel_17.setBackground(new Color(235, 219, 194));
 		panel_6.add(panel_17, BorderLayout.CENTER);
 		
-		JPanel panel_4 = new JPanel();
+		PainelArredondado panel_4 = new PainelArredondado();
 		panel_4.setBackground(new Color(235, 219, 194));
 		panel_1.add(panel_4, "cell 0 4,grow");
 		panel_4.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_12 = new JPanel();
+		PainelArredondado panel_12 = new PainelArredondado();
 		panel_12.setBackground(new Color(145, 131, 131));
 		panel_4.add(panel_12, BorderLayout.NORTH);
 		panel_12.setLayout(new CardLayout(0, 0));
@@ -167,16 +170,16 @@ public class TelaNotificacao extends JPanel {
 		panel_12.add(lbNo5, "name_9698410286500");
 		lbNo5.setFont(fonte2);
 		
-		JPanel panel_13 = new JPanel();
+		PainelArredondado panel_13 = new PainelArredondado();
 		panel_13.setBackground(new Color(235, 219, 194));
 		panel_4.add(panel_13, BorderLayout.CENTER);
 		
-		JPanel panel_7 = new JPanel();
+		PainelArredondado panel_7 = new PainelArredondado();
 		panel_7.setBackground(new Color(235, 219, 194));
 		panel_1.add(panel_7, "cell 2 4,grow");
 		panel_7.setLayout(new BorderLayout(0, 0));
 		
-		JPanel panel_18 = new JPanel();
+		PainelArredondado panel_18 = new PainelArredondado();
 		panel_18.setBackground(new Color(145, 131, 131));
 		panel_7.add(panel_18, BorderLayout.NORTH);
 		panel_18.setLayout(new CardLayout(0, 0));
@@ -187,7 +190,7 @@ public class TelaNotificacao extends JPanel {
 		panel_18.add(lbNo6, "name_9793065763000");
 		lbNo6.setFont(fonte2);
 		
-		JPanel panel_19 = new JPanel();
+		PainelArredondado panel_19 = new PainelArredondado();
 		panel_19.setBackground(new Color(235, 219, 194));
 		panel_7.add(panel_19, BorderLayout.CENTER);
 
